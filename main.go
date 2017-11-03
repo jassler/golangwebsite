dec6c92d02acb818c6d2d2914c896af89a67502e
Fritz Felix Wilhelm <FRIF@ESG-GMBH.DE>
2017-11-03 21:25:51 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func main() {
+	templates := populateTemplates()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		requestedFile := req.URL.Path[1:]
+		template := templates.Lookup(requestedFile + ".html")
+
+		if template != nil {
+			template.Execute(w, nil)
+		} else {
+			w.WriteHeader(404)
+		}
+	})
+
+	fmt.Println("v0.0.6 running")
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+
+	basePath := "templates"
+	templateFolder, _ := os.Open(basePath)
+	defer templateFolder.Close()
+
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths, basePath+"/"+pathInfo.Name())
+		}
+	}
+
+	result.ParseFiles(*templatePaths...)
+	return result
+}
